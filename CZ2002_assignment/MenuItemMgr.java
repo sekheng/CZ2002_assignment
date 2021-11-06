2b3d7be731b41c0f9bd92da2eaf61a595bb39292
KevinP25 <kevin.psantoso@gmail.com>
2021-11-06 23:02:26 +0800

    added data and exception handling

--- CZ2002_assignment/MenuItemMgr.java
+++ CZ2002_assignment/MenuItemMgr.java
@@ -2,112 +2,105 @@ package CZ2002_assignment;
 
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
+
+/**
+    To manage a list of menuItems
+    allows addition, updates, and deletion of menuItems
+    @author    Kevin Patrick Santoso
+    @version   1.0
+    @since     2021-06-11
+ */
 public class MenuItemMgr {
-    /**
-     * Constructs a new Scanner object
-     * sc contains the reference to the Scanner object
-     */
-    Scanner sc = new Scanner(System.in); //constructs a scanner for user input
 
     /**
-     * creates different ArrayList to contain menuItems and separate them according to their types
-     * appetizerList contains the reference to an ArrayList for menuItem type appetizer
-     * mainCourseList contains the reference to an ArrayList for menuItem type mainCourse
-     * drinkList contains the reference to an ArrayList for menuItem type drink
+     * creates an ArrayList called menuItemList to store all menu items
      */
-    private ArrayList<MenuItem> appetizerList = new ArrayList<MenuItem>();
-    private ArrayList<MenuItem> mainCourseList = new ArrayList<MenuItem>();
-    private ArrayList<MenuItem> drinkList = new ArrayList<MenuItem>();
-
-    
+    private ArrayList<MenuItem> menuItemList = new ArrayList<MenuItem>();
 
     /**
-     * this is a method to call the menuItem constructor
-     * with user input for menuItem values of the price, description, name, and type
-     * @param price will be input into the menuItem constructor price parameter
-     * @param description will be input into the menuItem constructor description parameter
-     * @param name will be input into the menuItem constructor name parameter
-     * @param type will be input into the menuItem constructor type parameter
-     * when all inputs are valid the switch will check for type and add it to correct menuItem ArrayList, and writes a message
+     * Constructor for a MenuItemMgr object
+     * when constructor is called, menuItemList will copy the ArrayList returned from MenuItemFactory
      */
-    public void createMenuItem(int price, String description, String name, String type){
-        MenuItem a = new MenuItem(price, description, name, type);
+    public MenuItemMgr()
+    {
+        try
+        {
+            menuItemList = MenuItemFactory.createMenuItem("MenuItemData.txt");
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
 
-        switch(type){
-            case "appetizer":
-            appetizerList.add(a);
-            System.out.println(a.getName() + " added to appetizerList");
+
+    /**
+     * adds a menu item to the menu item list
+     * @param price This menuItem's price
+     * @param description This menuItem's description
+     * @param name This menuItem's name
+     * @param type This menuItem's type
+     */
+    public void addMenuItem(int price, String description, String name, String type) throws InputMismatchException  {
+        if (price<0){
+            throw new ArithmeticException("price cannot be negative");
+        }
+
+        MenuItem temp = new MenuItem(price, description, name, type);
+        menuItemList.add(temp);
+    }
+
+
+    /**
+     * method to change the name, description, price, and type of a menuItem object
+     * @param item is the menu item to be updated
+     * we ask the users whcih attribute they want to change
+     * we print the old attribute and ask for a replacement
+     * @throws InputMismatchException
+     */
+    public void updateMenuItem(MenuItem item) throws InputMismatchException
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("(1) change name, (2) change price, (3) change description, (4) change type");
+        switch(sc.nextInt())
+        {
+            case 1:
+            System.out.println("old name is " + item.getName());
+            item.setName(sc.nextLine());
             break;
 
-            case "mainCourse":
-            case "maincourse":
-            mainCourseList.add(a);
-            System.out.println(a.getName() + " added to mainCourseList");
+            case 2:
+            System.out.println("old price is " + item.getPrice());
+            item.setPrice(sc.nextInt());
             break;
 
-            case "drink":
-            drinkList.add(a);
-            System.out.println(a.getName() + " added to drinkList");
+            case 3:
+            System.out.println("old description is " + item.getDescription());
+            item.setDescription(sc.nextLine());
+            break;
+
+            case 4:
+            System.out.println("old type is " + item.getType());
+            item.setType(sc.nextLine());
             break;
 
             default:
-            System.out.println("wrong menuItem type");
-            System.out.println("no item added");
+            System.out.println("that choice is not available");
             break;
-        }
-    }//end of createMenuItem
-
-
-    /**
-     * method to change the name, description, and price of a menuItem object
-     * we assume that the type will not change as menuItem type attribute will be used to select the correct ArrayList
-     * once the menuItem type is selected, we print the entire Arraylist for that menuItem type
-     * we then print a message to ask for user input for the ArrayList index to select the menuItem they want to update
-     * we then print out the old attributes of the menuItem object and ask for user input for new attribute values
-     */
-    public void updateMenuItem(Scanner sc){             //note in visual paradigm cant put ArrayList<MenuItem> into parameter
-        /* 
-        a.forEach(MenuItem -> {
-            System.out.println(MenuItem);
-        });
-*/
-        for (MenuItem a : appetizerList)
-        {
-            System.out.println(a);
-        }
-        /*
-        System.out.println("which item would you like to update");
-        int i = sc.nextInt();
-
-        System.out.println("old name is " + a.get(i+1).getName());
-        System.out.println("enter new name");
-        a.get(i+1).setName(sc.next());
-
-        System.out.println("old description is " + a.get(i+1).getDescription());
-        System.out.println("enter new description");
-        a.get(i+1).setDescription(sc.next());
-
-        System.out.println("old price is " + a.get(i+1).getPrice());
-        System.out.println("enter new price");
-        a.get(i+1).setPrice(sc.nextInt());
-
-        */
+        }// end of switch
+        sc.close();
     }//end of update
 
 
     /**
-     * method to remove a menuItem object from its type ArrayList
-     * we will use the menuItem type attribute to select the correct ArrayList
-     * we will print out the selected ArrayList
-     * we will then ask for user input for the index for the menuItem object they want to delete
+     * method to remove a menuItem object from the menuItemList
+     * @param i is the index for the menuItem object to be deleted
+     * deletes selected menuItem
+     * @throws ArrayIndexOutOfBoundsException
      */
-    public void removeMenuItem(ArrayList<MenuItem> a){
-        a.forEach(MenuItem -> {
-            System.out.println(MenuItem);
-        });
-
-        System.out.println("which item would you like to delete");
-        int i = sc.nextInt();
-        a.remove(i+1);
+    public void removeMenuItem(int i) throws ArrayIndexOutOfBoundsException
+    {
+        menuItemList.remove(i);
     }
 }
--- CZ2002_assignment/StaffMgr.java
+++ CZ2002_assignment/StaffMgr.java
@@ -1,6 +1,7 @@
 package CZ2002_assignment;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 
 /**
     To manage a list of Staffs
@@ -45,8 +46,9 @@ public class StaffMgr {
      * @param gender This staff's gender
      * @param employeeID This staff's employee ID
      * @param jobTitle This staff's job title
+     * @throws InputMismatchException
      */
-    public void addStaff(String name, int gender, String employeeID, String jobTitle){
+    public void addStaff(String name, int gender, String employeeID, String jobTitle) throws InputMismatchException{
         Staff temp = new Staff(name, gender, employeeID, jobTitle);
         staffList.add(temp);
     }
@@ -55,8 +57,9 @@ public class StaffMgr {
      * method to select a staff
      * @param i is the index for the staff to be selected
      * @return the staff selected
+     * @throws ArrayIndexOutofBoundsException
      */
-    public Staff selectStaff(int i)
+    public Staff selectStaff(int i) throws ArrayIndexOutOfBoundsException
     {
         currentStaff = staffList.get(i);
         return currentStaff;
