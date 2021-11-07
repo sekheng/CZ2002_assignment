68b82c51b314df69e93a5e266457b642abcc84b4
Kanupriya12 <kanupriyamalhotra12@gmail.com>
2021-11-07 21:30:11 +0800

    Create Order Class

--- CZ2002_assignment/Order.java
+++ CZ2002_assignment/Order.java
@@ -1,5 +1,28 @@
 package CZ2002_assignment;
 
-public class Order {
+import java.util.*;
 
+public class Order {
+	
+	private Staff CurrentStaff;
+	private int TableID;
+	private ArrayList<FoodItem>  arrayOfFoodItem;
+	
+	
+	public Order(Staff CurrentStaff, int TableID) {
+		this.CurrentStaff = CurrentStaff;
+		this.TableID = TableID;
+	}
+	
+	public Staff getStaff() {
+		return this.CurrentStaff;
+	}
+	
+	public ArrayList<FoodItem> getArrayOfFoodItem(){
+		return this.arrayOfFoodItem;
+	}
+	
+	public int getTableID() {
+		return this.TableID;
+	}
 }
