e275c27868618e7a1e58c210aa302a9132d9f311
Kanupriya12 <kanupriyamalhotra12@gmail.com>
2021-11-11 19:48:24 +0800

    changes made to order and ordermanager

--- CZ2002_assignment/OrderManager.java
+++ CZ2002_assignment/OrderManager.java
@@ -1,5 +1,63 @@
 package CZ2002_assignment;
 
+import java.util.ArrayList;
+import java.util.*;
+
+
 public class OrderManager {
 
+	private  ArrayList<Order> ArrayofOrders;
+	
+	public OrderManager(){
+		this.ArrayofOrders = ArrayofOrders;
+	}
+		
+	
+	public String addOrder(Order order){
+		ArrayofOrders.add(order);
+		String confirmation = "Successfully added Order";
+		return confirmation;
+		
+	}
+	
+	public String removeOrder(int TableID) {
+		int flag =0;
+		String cancellation;
+		for(int i =0; i<ArrayofOrders.size();i++) {
+			if(ArrayofOrders.get(i).getTableID() == TableID) {
+				ArrayofOrders.remove(i);
+				flag=1;
+			}
+		}
+
+		if(flag == 0) cancellation = "No order found for this tableID";
+		else cancellation = "Order Cancelled";
+		return cancellation;
+	}
+	
+	public ArrayList<Order> getArrayofOrder(){
+		return this.ArrayofOrders;
+	}
+	
+	public Order getOrder(int TableID) {
+		int flag = 0;
+		Order selectedOrder =null;
+		
+		// Search for Order by tableID 
+		for(int i =0; i<ArrayofOrders.size();i++) {
+			if(ArrayofOrders.get(i).getTableID() == TableID) {
+				flag =1;
+				selectedOrder = ArrayofOrders.get(i);
+			}
+		}
+		
+		//Check whether tableID existed in ArrayofOrders 
+		if(flag == 0) {
+			System.out.println("No such tableID has been assigned an Order");
+		}
+		
+		return selectedOrder;
+	}
+
+	
 }
