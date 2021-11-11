91856b339abbabcf8370c428f79058a0da52cc07
Kanupriya12 <kanupriyamalhotra12@gmail.com>
2021-11-11 20:46:42 +0800

    Update OrderInvoice

--- CZ2002_assignment/OrderInvoice.java
+++ CZ2002_assignment/OrderInvoice.java
@@ -1,25 +1,40 @@
 package CZ2002_assignment;
 
 import java.util.*;
+import java.time.LocalDateTime;
 
 public class OrderInvoice {
 	
 	private float tax;
 	private float discount;
-	private Date localDateTime;
-	private Order AttachedOrder;
+	private LocalDateTime currentTime;
+	private Order attachedOrder;
 	
-	public float getTotalCost(Order AttachedOrder, float tax, float discount) {
+	public OrderInvoice(Order attachedOrder, LocalDateTime currentTime, float discount, float tax) {
+		this.attachedOrder = attachedOrder;
+		this.currentTime = currentTime;
+		this.discount = discount;
+		this.tax = tax;
+	}
+	
+	
+	public double getTotalCost(Order AttachedOrder, float tax, float discount) {
 		ArrayList<FoodItem> arrayOfFoodItems = AttachedOrder.getArrayOfFoodItem();
 		float cost =0;
 		for(int i=0;i<arrayOfFoodItems.size();i++) {
 			cost=cost+arrayOfFoodItems.get(i).getPriceInCents();
 		}
-		
-		cost= cost+cost*(tax/100);
+		cost= cost+(cost*(tax/100));
 		cost = cost - discount*cost;
 		return cost;
 	}
+	
+	
+	public void PrintReceipt() {
+		double cost = this.getTotalCost(this.attachedOrder,this.tax,this.discount);
+		System.out.println("")
+	}
+	
 	
 	
 	
