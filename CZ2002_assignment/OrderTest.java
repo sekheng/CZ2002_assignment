5ea8bf3d232338fd40abad7bcb2cc31f62503578
Kanupriya12 <kanupriyamalhotra12@gmail.com>
2021-11-12 21:31:27 +0800

    formatting changes

--- CZ2002_assignment/OrderTest.java
+++ CZ2002_assignment/OrderTest.java
@@ -31,13 +31,13 @@ public class OrderTest {
 		
 		
 		OrderInvoice testinvoice = new OrderInvoice(testOrder1, LocalDateTime.now(), 0, 0);
-		testManager.printReceipt(8, 3, 2);
+//		testManager.printReceipt(8, 3, 2);
 		
 		RevenueReport revenueReport = testManager.getRevenueReport();
 		LocalDateTime startDate = LocalDateTime.parse("2016-10-31 23:59:59", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
 		LocalDateTime endDate = LocalDateTime.parse("2023-10-31 23:59:59", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
 		
-		revenueReport.GetOrderInvoice(startDate, endDate);
+//		revenueReport.GetOrderInvoice(startDate, endDate);
 		
 	}
 
